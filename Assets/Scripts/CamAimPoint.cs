67fe5182d1f4bff8ed515c5c4c79f12ad4ebf877
Poio <jesuslebarbu@yahoo.fr>
2019-04-27 22:11:47 +0200

    Camera stuff

---
+++ Assets/Scripts/CamAimPoint.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamAimPoint : MonoBehaviour
+{
+    public Rigidbody2D protag;
+    public float lookForwardFactor = 3f;
+
+    public Transform leftBorder;
+    public Transform rightBorder;
+    public Transform bottomBorder;
+    public Transform topBorder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 unclampedAim = protag.position + protag.velocity.normalized * lookForwardFactor;
+
+        Vector3 minXY = Camera.main.ViewportToWorldPoint(new Vector3(-1, -1, 0));
+        Vector3 maxXY = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
+        Vector3 diff = (maxXY - minXY)/4f;
+
+        Vector2 clampedAim = unclampedAim;
+
+        if (unclampedAim.y < bottomBorder.transform.position.y + diff.y )
+            clampedAim.y = bottomBorder.transform.position.y + diff.y;
+
+        if (unclampedAim.y > topBorder.transform.position.y - diff.y)
+            clampedAim.y = topBorder.transform.position.y - diff.y;
+
+        if (unclampedAim.x < leftBorder.transform.position.x + diff.x)
+            clampedAim.x = leftBorder.transform.position.x + diff.x;
+
+        if (unclampedAim.x > rightBorder.transform.position.x - diff.x)
+            clampedAim.x = rightBorder.transform.position.x - diff.x;
+
+        transform.position = new Vector3(clampedAim.x, clampedAim.y, 0);
+    }
+}
---
+++ Assets/Scripts/CamScroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamScroller : MonoBehaviour
+{
+    public GameObject aimPoint;
+    public float smoothSpeed = 0.125f;
+    public Vector3 zOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aimPoint.transform.position = transform.position - zOffset;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 aim = aimPoint.transform.position + zOffset;
+        transform.position = Vector3.Lerp(transform.position, aim, smoothSpeed);
+    }
+}
