83f405ae5e13ff382a6527fb2198b934f71ea876
Darfeld <darfeld@gmail.com>
2019-04-27 17:02:01 +0200

    Move right or Left

--- Assets/Scripts/Perso_controler.cs
+++ Assets/Scripts/Perso_controler.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Perso_controler : MonoBehaviour
 {
+
+    public float maxSpeed = 10f;
+    public bool facingRight = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +15,25 @@ public class Perso_controler : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float move = Input.GetAxis("Horizontal");
+
+        Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>() ;
+
+        rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+        if ((move > 0 && !facingRight) || (move < 0 && facingRight))
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
