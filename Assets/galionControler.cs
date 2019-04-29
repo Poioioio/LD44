045375d32279124591c97e702e7491a5ea634fa1
Darfeld <darfeld@gmail.com>
2019-04-30 01:51:59 +0200

    Ajoute musiques d'ambiance et de gallion

---
+++ Assets/galionControler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class galionControler : MonoBehaviour
+{
+
+    AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (source.isPlaying)
+            {
+                source.Stop();
+            }
+            else
+            {
+                source.enabled = true;
+                source.Play();
+            }
+        }
+    }
+}
