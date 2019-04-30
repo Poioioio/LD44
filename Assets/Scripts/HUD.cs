f9c543f79c73b2af8c0d1394ca53741517a7b308
Poio <jesuslebarbu@yahoo.fr>
2019-04-30 13:06:17 +0200

    v1.2 : galleon's collider updated, more coins, hud tweaks

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -24,7 +24,9 @@ public class HUD : MonoBehaviour
         }
     }
     #endregion
-    AudioSource coinSound;
+    AudioSource audioSource;
+    public AudioClip coinSound;
+    public AudioClip evilLaugh;
 
     public Text copperText;
     public Image copperImage;
@@ -38,95 +40,116 @@ public class HUD : MonoBehaviour
     public Image showGold;
     public RawImage intro;
     public RawImage gameOver;
-    public RawImage victory;
+    public Image victory;
 
     private bool firstCopper = true;
     private bool firstSilver = true;
     private bool firstGold = true;
+
+    Perso_controler protag;
 
 
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
-        coinSound = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
+
+        protag = FindObjectOfType<Perso_controler>();
+        protag.enabled = false;
+
         intro.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.R))
         {
             Time.timeScale = 1f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        else if ( Input.anyKeyDown )
+        {
+            Time.timeScale = 1f;
+            if( gameOver.gameObject.activeInHierarchy )
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
             showCopper.gameObject.SetActive(false);
             showSilver.gameObject.SetActive(false);
             showGold.gameObject.SetActive(false);
             intro.gameObject.SetActive(false);
             gameOver.gameObject.SetActive(false);
             victory.gameObject.SetActive(false);
-        }
 
-        if(Input.GetKeyDown(KeyCode.R))
-        {
-            Time.timeScale = 1f;
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
+            StartCoroutine(ReEnableProtag());
+        }        
     }
+
+    IEnumerator ReEnableProtag()
+    {
+        yield return new WaitForSeconds(0.2f);
+        protag.enabled = true;
+    }
+
 
     public void UpdateCopperCoin(int nb)
     {
+        bool gotSome = nb > 0;
+        if( gotSome )
+            audioSource.PlayOneShot(coinSound);
+
         if (firstCopper)
         {
             firstCopper = false;
             showCopper.gameObject.SetActive(true);
+            protag.enabled = false;
             Time.timeScale = 0.01f;
         }
 
-        bool gotSome = nb > 0;
         copperText.gameObject.SetActive(gotSome);
         copperImage.gameObject.SetActive(gotSome);
         copperText.text = "Tibet coins : " + nb.ToString();
 
-        coinSound.enabled = true;
-        if( gotSome )
-            coinSound.Play();
     }
 
     public void UpdateSilverCoin(int nb)
     {
+        bool gotSome = nb > 0;
+        if (gotSome)
+            audioSource.PlayOneShot(coinSound);
+
         if (firstSilver)
         {
             firstSilver = false;
             showSilver.gameObject.SetActive(true);
+            protag.enabled = false;
             Time.timeScale = 0.01f;
         }
 
-        bool gotSome = nb > 0;
         silverText.gameObject.SetActive(gotSome);
         silverImage.gameObject.SetActive(gotSome);
         silverText.text = "Drachmes : " + nb.ToString();
 
-        coinSound.enabled = true;
-        coinSound.Play();
     }
 
     public void UpdateGoldCoin(int nb)
     {
+        bool gotSome = nb > 0;
+        if (gotSome)
+            audioSource.PlayOneShot(coinSound);
         if (firstGold)
         {
             firstGold = false;
             showGold.gameObject.SetActive(true);
+            protag.enabled = false;
             Time.timeScale = 0.01f;
         }
 
-        bool gotSome = nb > 0;
         goldText.gameObject.SetActive(gotSome);
         goldImage.gameObject.SetActive(gotSome);
         goldText.text = "Stateres : " + nb.ToString();
 
-        coinSound.enabled = true;
-        coinSound.Play();
     }
 
     public void ShowVictory()
@@ -135,9 +158,11 @@ public class HUD : MonoBehaviour
         victory.gameObject.SetActive(true);
     }
 
-    public void ShowGameOver()
+    public void ShowGameOver(bool laugh = false)
     {
         Time.timeScale = 0f;
+        audioSource.PlayOneShot(evilLaugh);
+        protag.enabled = false;
         gameOver.gameObject.SetActive(true);
     }
 }
